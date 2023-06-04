e2cdc7012852491881a87223cd770bdf82a7f641
pengolodh <lysander.deloore@gmail.com>
2023-06-04 22:59:44 +0200

    fixed wireguard

--- hosts/nixos-macbook/core/networking.nix
+++ hosts/nixos-macbook/core/networking.nix
@@ -49,35 +49,18 @@
     # ---
 
     # set wireguard config
-    wireguard.interfaces = {
+    wg-quick.interfaces = {
       wg0 = {
-        # Determines the IP address and subnet of the client's end of the tunnel interface.
-        ips = [ "172.16.0.4/32" ];
-        listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
-
-        # Path to the private key file.
-        #
-        # Note: The private key can also be included inline via the privateKey option,
-        # but this makes the private key world-readable; thus, using privateKeyFile is
-        # recommended.
-        privateKeyFile = "../../../.secrets/wireguard/wg_nixos-macbook.key";
+        address = [ "172.16.0.4/32" ];
+        dns = [ "1.1.1.1" ];
+        privateKeyFile = "/root/wireguard-keys/wg_macbook-nixos.key";
 
         peers = [
-          # For a client configuration, one peer entry for the server will suffice.
-
           {
-            # Public key of the server (not a file path).
             publicKey = "TiW1dNArzZIudtHrtIYXHtogDNRZY4lGjfeNw1qJ9jk=";
-
-            # Forward all the traffic via VPN.
-            allowedIPs = [ "0.0.0.0/0" ];
-            # Or forward only particular subnets
-            #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
-
-            # Set this to the server IP and port.
-            endpoint = "78.22.99.166:13231"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
-
-            # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+            #presharedKeyFile = "/root/wireguard-keys/preshared_from_peer0_key";
+            allowedIPs = [ "0.0.0.0/0"];
+            endpoint = "78.22.99.166:13231";
             persistentKeepalive = 25;
           }
         ];
