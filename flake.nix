7205f85647fa0e82129fcce2792b250de9e2095e
pengolodh <lysander.deloore@gmail.com>
2023-12-07 12:07:22 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         hyprland.url = "github:hyprwm/Hyprland";
         impermanence.url = "github:nix-community/impermanence";
         sops-nix = {
-            url = "github:Nic92/sops-nix";
+            url = "github:Mic92/sops-nix";
             inputs.nixpkgs.follows = "nixpkgs";
         };
         # plasma-manager
