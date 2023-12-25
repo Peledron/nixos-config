3a746b91f280ad59bccd3f0ebf20b88e89892670
pengolodh <lysander.deloore@gmail.com>
2023-12-25 15:49:16 +0100

    exa is depcricated, removed most pkgs from pengolodh-server pkgs.nix

--- hosts/global/users/pengolodh/home/server/pkgs.nix
+++ hosts/global/users/pengolodh/home/server/pkgs.nix
@@ -1,17 +1,7 @@
 { config, lib, pkgs, system, imputs, ... }:
 {
     home.packages = with pkgs; [
-            # cli tools
-                # [shells]
-                fish
-                # [shell prompts]
-                starship
-                # [util replacements]
-                bat # cat replacement with syntax highlighting, etc
-                exa # colorfull ls, easier to read
-                # [utils]
-                #steam-run # allows you to run commands in the steam runtime env, usefull for running binaries that need FSH compatibility (not pure tho)
-                # --> steam-run will keep steam installed
+                starshipLw
         ];
 
         #==================#
