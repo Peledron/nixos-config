278e66fbbfb7a35da903eab69f4789d077d36e77
pengolodh <lysander.deloore@gmail.com>
2024-01-25 15:12:00 +0100

    main hyprland

--- hosts/nixos-main/core/secrets.nix
+++ hosts/nixos-main/core/secrets.nix
@@ -1,6 +1,7 @@
 {config, lib, pkgs, self, sops, ...}:
 {
     sops = {
+        gnupg.sshKeyPaths = [];
         # import host ssh key as age key
         age.sshKeyPaths = lib.mkForce [ "/persist/ssh/ssh_host_ed25519_key" ];
     };
