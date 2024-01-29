e3940f08c4c7eba5819f260ce565b5bf27807a60
pengolodh <lysander.deloore@gmail.com>
2024-01-29 12:55:27 +0100

    added kvantum theming

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,20 +1,25 @@
 # declare the hosts for the flake, default.nix will always be used when importing a directory
-{ lib, self, inputs, ... }:
-let
+{
+  lib,
+  self,
+  inputs,
+  ...
+}: let
   # package modules
   system = "x86_64-linux"; # System architecture
   lib = inputs.nixpkgs.lib;
 
   pkgs = import inputs.nixpkgs {
     inherit system;
-    config.allowUnfree = true;  # Allow proprietary software
+    config.allowUnfree = true; # Allow proprietary software
     overlays = [
       inputs.nur.overlay # overlay nixpkgs with nur, meant that you add nur packages to the nixpkgs module (I think)
     ];
   };
 
-  nur-no-pkgs = import inputs.nur { # so we can import overlapping modules
-    nurpkgs = import inputs.nixpkgs {inherit system; };
+  nur-no-pkgs = import inputs.nur {
+    # so we can import overlapping modules
+    nurpkgs = import inputs.nixpkgs {inherit system;};
   };
 
   # aditional functionality
@@ -43,7 +48,7 @@ let
   desktop_envdir = "${global-desktopdir}/environments";
 
   ## => hyprland
-  hyprland-coreconf  = "${desktop_envdir}/hyprland.nix";
+  hyprland-coreconf = "${desktop_envdir}/hyprland.nix";
   hyprland-homeconf = "${desktop_envdir}/hyprland/home.nix";
 
   ## => sway
@@ -65,57 +70,55 @@ let
   pengolodh-coreconf = "${global-usrdir}/pengolodh/usr.nix";
   pengolodh_desktop-homeconf = "${global-usrdir}/pengolodh/home/desktop/home.nix";
   pengolodh_server-homeconf = "${global-usrdir}/pengolodh/home/server/home.nix";
-
-in
-
-{
+in {
   #==================#
   # vm-desktop:
   #==================#
   vm-nixos-desktop = lib.nixosSystem {
     inherit system pkgs;
     specialArgs = {
-      inherit inputs self; 
+      inherit inputs self;
     };
     modules = [
-        # inputs
-        sops
-        disko
-        impermanence
+      # inputs
+      sops
+      disko
+      impermanence
 
-        # modules
-        global-coreconf
-        global-desktopconf
-        sway-coreconf
+      # modules
+      global-coreconf
+      global-desktopconf
+      sway-coreconf
 
-        # -> host module
-        "${hostdir}/vm-nixos-desktop" {
-          _module.args.disks = [ "/dev/vda" ]; # change this to sda for vmware, you can add more drives in more "", for example "/dev/nvme0n1"
-        }
+      # -> host module
+      "${hostdir}/vm-nixos-desktop"
+      {
+        _module.args.disks = ["/dev/vda"]; # change this to sda for vmware, you can add more drives in more "", for example "/dev/nvme0n1"
+      }
 
-        # -> hardware
-        "${hostdir}/vm-nixos-desktop/core/hardwareqemu.nix" # change this to hardwarevmware.nix for vmware
+      # -> hardware
+      "${hostdir}/vm-nixos-desktop/core/hardwareqemu.nix" # change this to hardwarevmware.nix for vmware
 
-        # -> user modules
-        pengolodh-coreconf
+      # -> user modules
+      pengolodh-coreconf
+      # add more users here:
+
+      #==================#
+      # system home-man:
+      home-manager
+      {
+        home-manager.useGlobalPkgs = true; # sets home-manager to use the nix-package-manager packages instead of its own internal ones
+        home-manager.useUserPackages = true; # packages will be installed per user;
+        home-manager.extraSpecialArgs = {};
+        home-manager.users.pengolodh = {
+          imports =
+            [pengolodh_desktop-homeconf]
+            ++ [sway-homeconf]; # add more inports via [import module] ++ (import folder) or ++ [(import file)]
+        };
+        # ---
         # add more users here:
-
-        #==================#
-        # system home-man:
-        home-manager {
-          home-manager.useGlobalPkgs = true; # sets home-manager to use the nix-package-manager packages instead of its own internal ones
-          home-manager.useUserPackages = true; # packages will be installed per user;
-          home-manager.extraSpecialArgs = {  };
-          home-manager.users.pengolodh = {
-            imports = 
-              [pengolodh_desktop-homeconf]
-              ++ [sway-homeconf]
-            ; # add more inports via [import module] ++ (import folder) or ++ [(import file)]
-          };
-          # ---
-          # add more users here:
-        }
-      ];
+      }
+    ];
   };
   # ---
 
@@ -130,50 +133,58 @@ in
   nixos-main = lib.nixosSystem {
     inherit system pkgs;
     specialArgs = {
-      inherit inputs self; 
+      inherit inputs self;
     };
     modules = [
-        # inputs
-        sops
-        disko
-        impermanence
-        #hyprland-coremod
+      # inputs
+      sops
+      disko
+      impermanence
+      #hyprland-coremod
 
-        # modules
-        global-coreconf
-        global-desktopconf
-        #hyprland-coreconf
-        kde-coreconf
-        
-        # -> host module
-        "${hostdir}/nixos-main" {
-          _module.args.disks = [ 
-            "/dev/disk/by-id/nvme-SAMSUNG_MZVLW512HMJP-000H1_S36ENX0HA25227" # 512GB root drive
-            "/dev/disk/by-id/nvme-SAMSUNG_MZVLB1T0HALR-00000_S3W6NX0N701285" # 1TB home drive
-          ]; 
-        }
+      # modules
+      global-coreconf
+      global-desktopconf
+      #hyprland-coreconf
+      kde-coreconf
 
-        # -> user modules
-        pengolodh-coreconf
+      # -> host module
+      "${hostdir}/nixos-main"
+      {
+        _module.args.disks = [
+          # system disks
+          "/dev/disk/by-id/nvme-SAMSUNG_MZVLW512HMJP-000H1_S36ENX0HA25227" # 512GB root drive
+          "/dev/disk/by-id/nvme-SAMSUNG_MZVLB1T0HALR-00000_S3W6NX0N701285" # 1TB home drive
+          # existing system partitions
+          "/dev/disk/by-id/nvme-Samsung_SSD_980_PRO_2TB_S69ENX0TB18294T-part3" # 2 TB windows root partition
+          # existing data partitions
+          "/dev/mapper/big--data-data--games" # 4TB linux-game drive (luks on lvm)
+          "/dev/disk/by-id/ata-TOSHIBA_DT01ACA300_95QGT6KGS-part2" # 3tb windows-data drive (ntfs)
+          "/dev/disk/by-id/ata-ST1000DM003-1ER162_Z4YC0ZWB-part1" #1TB windows-mod drive (ntfs)
+        ];
+      }
+
+      # -> user modules
+      pengolodh-coreconf
+      # add more users here:
+
+      #==================#
+      # system home-man:
+      home-manager
+      {
+        home-manager.useGlobalPkgs = true; # sets home-manager to use the nix-package-manager packages instead of its own internal ones
+        home-manager.useUserPackages = true; # packages will be installed per user;
+        home-manager.extraSpecialArgs = {inherit self;};
+        home-manager.users.pengolodh = {
+          imports =
+            [plasma-manager]
+            ++ [pengolodh_desktop-homeconf]
+            ++ [kde-homeconf]; # add more inports via [import module] ++ (import folder) or ++ [(import file)]
+        };
+        # ---
         # add more users here:
-
-        #==================#
-        # system home-man:
-        home-manager {
-          home-manager.useGlobalPkgs = true; # sets home-manager to use the nix-package-manager packages instead of its own internal ones
-          home-manager.useUserPackages = true; # packages will be installed per user;
-          home-manager.extraSpecialArgs = { inherit self; };
-          home-manager.users.pengolodh = {
-            imports = 
-              [plasma-manager]
-              ++ [pengolodh_desktop-homeconf]
-              ++ [kde-homeconf]
-            ; # add more inports via [import module] ++ (import folder) or ++ [(import file)]
-          };
-          # ---
-          # add more users here:
-        }
-      ];
+      }
+    ];
   };
 
   # ---
@@ -183,91 +194,83 @@ in
       inherit inputs self;
     };
     modules = [
-        # inputs
-        sops
-        hyprland-coremod
+      # inputs
+      sops
+      hyprland-coremod
 
-        # modules
-        global-coreconf
-        hyprland-coreconf
+      # modules
+      global-coreconf
+      hyprland-coreconf
 
-        # -> host module
-        "${hostdir}/nixos-macbook"
+      # -> host module
+      "${hostdir}/nixos-macbook"
 
-        # -> user modules
-        pengolodh-coreconf
-        
-        #==================#
-        # system home-man:
-        home-manager {
-          home-manager.useGlobalPkgs = true; # sets home-manager to use the nix-package-manager packages instead of its own internal ones
-          home-manager.useUserPackages = true; # packages will be installed per user;
-          home-manager.extraSpecialArgs = {
-            inherit (inputs);
-          };
-          home-manager.users.pengolodh = {
-            imports = 
-              #[plasma-manager]  # add plasma-manager to home-man user imports as per https://github.com/pjones/plasma-manager/issues/5
-              [hyprland-homemod]
-               ++ [hyprland-homeconf]
-               # ++ [kde-homeconf]
-              ++ [pengolodh_desktop-homeconf]
-            ; # add more inports via [import module] ++ (import folder) or ++ [(import file)]
-            
-          };
-          # ---
-          # add more users here:
-        }
-	
-        
-        
-      ];
+      # -> user modules
+      pengolodh-coreconf
+
+      #==================#
+      # system home-man:
+      home-manager
+      {
+        home-manager.useGlobalPkgs = true; # sets home-manager to use the nix-package-manager packages instead of its own internal ones
+        home-manager.useUserPackages = true; # packages will be installed per user;
+        home-manager.extraSpecialArgs = {
+          inherit (inputs);
+        };
+        home-manager.users.pengolodh = {
+          imports =
+            #[plasma-manager]  # add plasma-manager to home-man user imports as per https://github.com/pjones/plasma-manager/issues/5
+            [hyprland-homemod]
+            ++ [hyprland-homeconf]
+            # ++ [kde-homeconf]
+            ++ [pengolodh_desktop-homeconf]; # add more inports via [import module] ++ (import folder) or ++ [(import file)]
+        };
+        # ---
+        # add more users here:
+      }
+    ];
   };
   # ---
-   nixos-laptop-asus = lib.nixosSystem {
+  nixos-laptop-asus = lib.nixosSystem {
     inherit system pkgs;
     specialArgs = {
       inherit inputs self;
     };
     modules = [
-        # inputs
-        sops
-        #hyprland-coremod
+      # inputs
+      sops
+      #hyprland-coremod
 
-        # modules
-        global-coreconf
-        kde-coreconf
+      # modules
+      global-coreconf
+      kde-coreconf
 
-        # -> host module
-        "${hostdir}/nixos-laptop-asus"
+      # -> host module
+      "${hostdir}/nixos-laptop-asus"
 
-        # -> user modules
-        pengolodh-coreconf
-        
-        #==================#
-        # system home-man:
-        home-manager {
-          home-manager.useGlobalPkgs = true; # sets home-manager to use the nix-package-manager packages instead of its own internal ones
-          home-manager.useUserPackages = true; # packages will be installed per user;
-          home-manager.extraSpecialArgs = {
-            inherit (inputs);
-          };
-          home-manager.users.pengolodh = {
-            imports = 
-              # [hyprland-homemod]
-              [plasma-manager]  # add plasma-manager to home-man user imports as per https://github.com/pjones/plasma-manager/issues/5
-              ++ [kde-homeconf]
-              ++ [pengolodh_desktop-homeconf]
-            ; # add more inports via [import module] ++ (import folder) or ++ [(import file)]
-            
-          };
-          # ---
-          # add more users here:
-        }
-	
-        
-        
-      ];
+      # -> user modules
+      pengolodh-coreconf
+
+      #==================#
+      # system home-man:
+      home-manager
+      {
+        home-manager.useGlobalPkgs = true; # sets home-manager to use the nix-package-manager packages instead of its own internal ones
+        home-manager.useUserPackages = true; # packages will be installed per user;
+        home-manager.extraSpecialArgs = {
+          inherit (inputs);
+        };
+        home-manager.users.pengolodh = {
+          imports =
+            # [hyprland-homemod]
+            [plasma-manager] # add plasma-manager to home-man user imports as per https://github.com/pjones/plasma-manager/issues/5
+            ++ [kde-homeconf]
+            ++ [pengolodh_desktop-homeconf]; # add more inports via [import module] ++ (import folder) or ++ [(import file)]
+        };
+        # ---
+        # add more users here:
+      }
+    ];
   };
 
   #==================#
@@ -278,44 +281,40 @@ in
     specialArgs = {
       inherit inputs self;
     };
-      modules = [
-        # inputs
-        sops
-        disko
-        impermanence
+    modules = [
+      # inputs
+      sops
+      disko
+      impermanence
 
-        # modules
-        global-coreconf
+      # modules
+      global-coreconf
 
-        # -> host module
-        "${hostdir}/nixos-server-hp"  {
-          _module.args.disks = [ "/dev/disk/by-id/ata-SanDisk_SD8SBAT128G1002_162092404193" "/dev/disk/by-id/ata-SanDisk_SD8SBAT128G1002_162092404193-part1" ]; # you can add more drives in more "", for example "/dev/nvme0n1", or you can specifiy partitions
-          _module.args.netport = "eno1"; # the physical ethernet port
-          _module.args.vlans = [ 112 113 114 ]; # the incoming vlan tags
-        }
+      # -> host module
+      "${hostdir}/nixos-server-hp"
+      {
+        _module.args.disks = ["/dev/disk/by-id/ata-SanDisk_SD8SBAT128G1002_162092404193" "/dev/disk/by-id/ata-SanDisk_SD8SBAT128G1002_162092404193-part1"]; # you can add more drives in more "", for example "/dev/nvme0n1", or you can specifiy partitions
+        _module.args.netport = "eno1"; # the physical ethernet port
+        _module.args.vlans = [112 113 114]; # the incoming vlan tags
+      }
 
-        # -> user modules
-        pengolodh-coreconf
+      # -> user modules
+      pengolodh-coreconf
 
-        #==================#
-        # system home-man:
-        home-manager {
-          home-manager.useGlobalPkgs = true; # sets home-manager to use the nix-package-manager packages instead of its own internal ones
-          home-manager.useUserPackages = true; # packages will be installed per user;
-          home-manager.extraSpecialArgs = {  };
-          home-manager.users.pengolodh = {
-            imports =
-              [pengolodh_server-homeconf]
-            ; # add more inports via [import module] ++ (import folder) or ++ [(import file)]
-
-          };
-          # ---
-          # add more users here:
-        }
-
-
-
-      ];
+      #==================#
+      # system home-man:
+      home-manager
+      {
+        home-manager.useGlobalPkgs = true; # sets home-manager to use the nix-package-manager packages instead of its own internal ones
+        home-manager.useUserPackages = true; # packages will be installed per user;
+        home-manager.extraSpecialArgs = {};
+        home-manager.users.pengolodh = {
+          imports = [pengolodh_server-homeconf]; # add more inports via [import module] ++ (import folder) or ++ [(import file)]
+        };
+        # ---
+        # add more users here:
+      }
+    ];
   };
   # ---
 
--- hosts/nixos-main/core/filesystems.nix
+++ hosts/nixos-main/core/filesystems.nix
@@ -1,8 +1,13 @@
 # drive config
-
-{ config, lib, pkgs, disko, disks, ... }:
 {
-  # using zfs, following from https://www.reddit.com/r/NixOS/comments/ruyunj/comment/hr4lijv/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button 
+  config,
+  lib,
+  pkgs,
+  disko,
+  disks,
+  ...
+}: {
+  # using zfs, following from https://www.reddit.com/r/NixOS/comments/ruyunj/comment/hr4lijv/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
   disko.devices = {
     disk.nixos-root = {
       device = builtins.elemAt disks 0;
@@ -11,16 +16,16 @@
         type = "gpt";
         partitions = {
           ESP = {
-              name = "NIXOS_EFI";
-              size = "512M";
-              type = "EF00"; # efi partition type
-              content = {
-                # here we will tell it the filesystem type and mountpoint
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "defaults" ];
-              };
+            name = "NIXOS_EFI";
+            size = "512M";
+            type = "EF00"; # efi partition type
+            content = {
+              # here we will tell it the filesystem type and mountpoint
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = ["defaults"];
+            };
           };
           cr_nixos-main = {
             size = "60G";
@@ -32,7 +37,7 @@
               # no keyfile will be specified as there will only be a password for this disk
               content = {
                 type = "filesystem";
-                format = "xfs"; 
+                format = "xfs";
                 mountpoint = "/nix";
               };
             };
@@ -50,19 +55,18 @@
               type = "luks";
               name = "cr_nixos-persist";
               passwordFile = "/tmp/nixos-main.passwd"; # the password will be the same as /nix, this will only prompt for 1 password and reuse the given one at boot
-              additionalKeyFiles = [ "/tmp/nixos-persist.key" ];
-              settings= {
+              additionalKeyFiles = ["/tmp/nixos-persist.key"];
+              settings = {
                 allowDiscards = true;
-                #keyFile = "/nix/keys/nixos-persist.key"; # generated using openssl-genrsa -out 
+                #keyFile = "/nix/keys/nixos-persist.key"; # generated using openssl-genrsa -out
               };
               content = {
                 type = "filesystem";
-                format = "xfs"; 
+                format = "xfs";
                 mountpoint = "/persist";
               };
             };
           };
-        
         };
       };
     };
@@ -78,19 +82,19 @@
               type = "luks";
               name = "cr_home";
               passwordFile = "/tmp/data-home.passwd";
-              additionalKeyFiles = [ "/tmp/data-home.key" ];
-              settings = { 
+              additionalKeyFiles = ["/tmp/data-home.key"];
+              settings = {
                 allowDiscards = true;
                 #keyFile = "/nix/keys/data-home.key"; # path to the disk encryption key (for boot)
               };
               initrdUnlock = false; # do not add this drive to the initrd devices mounted during boot, we will do this in stage 2 using systemd crypttab file instead (see below)
               content = {
                 type = "btrfs";
-                extraArgs = [ "-f" ]; # force create the partition
+                extraArgs = ["-f"]; # force create the partition
                 subvolumes = {
                   "/home" = {
                     mountpoint = "/home";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = ["compress=zstd" "noatime"];
                   };
                   "/home/pengolodh" = {}; # /home is mounted, so home/user does not need to be (it acts as a folder, I assume that the compression will still be applied to the sub-subvolume)
                 };
@@ -100,7 +104,7 @@
         };
       };
     };
-      
+
     nodev."/" = {
       fsType = "tmpfs";
       mountOptions = [
@@ -116,6 +120,30 @@
     "/".neededForBoot = true;
     "/nix".neededForBoot = true;
     "/persist".neededForBoot = true;
+    "/home/pengolodh/Games" = {
+      device = "/dev/mapper/cr_games"; # see below for the crypttab configuration
+      fsType = "ext4";
+      options = ["defaults" "noatime" "nofail"];
+      depends = ["home"];
+    };
+    "/home/pengolodh/Data/windows/windows-root" = {
+      device = builtins.elemAt disks 2;
+      fsType = "ntfs";
+      options = ["defaults" "noatime" "nofail"];
+      depends = ["/home"];
+    };
+    "/home/pengolodh/Data/windows/windows-data-main" = {
+      device = builtins.elemAt disks 4;
+      fsType = "ntfs";
+      options = ["defaults" "noatime" "nofail"];
+      depends = ["/home"];
+    };
+    "/home/pengolodh/Data/windows/windows-data-mods" = {
+      device = builtins.elemAt disks 5;
+      fsType = "ntfs";
+      options = ["defaults" "noatime" "nofail"];
+      depends = ["/home"];
+    };
   };
 
   # using crypttab will allow systemd to auto-mount the devices on stage2 of the boot process (after initrd and nixos mounts are done), this should work...
@@ -123,14 +151,15 @@
     enable = true;
     text = ''
       cr_home ${builtins.elemAt disks 1}-part1 /nix/keys/data-home.key luks,discard
+      cr_games ${builtins.elemAt disks 3} /nix/keys/data-games.key luks
     '';
-  }; 
+  };
   # ---
 
   # we are using btrfs so we can enable the scrub service here, as it is filesystem dependant
   services.btrfs.autoScrub = {
     enable = true;
     interval = "weekly";
-    fileSystems = [ "/home" ]; # does not need to be done on the nested sub-volumes
+    fileSystems = ["/home"]; # does not need to be done on the nested sub-volumes
   };
 }
