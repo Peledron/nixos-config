01e0bc4e5bc8b6dc208239b3b7e25ee53540c186
pengolodh <lysander.deloore@gmail.com>
2024-02-09 19:14:48 +0100

    added looking-glass-client to global desktop-env packages, not sure if it should be there or somewhere else

--- hosts/global/config/desktop/system/pkgs.nix
+++ hosts/global/config/desktop/system/pkgs.nix
@@ -7,6 +7,7 @@
   # globally installed packages related to desktop use
   environment.systemPackages = with pkgs; [
     virt-manager
+    looking-glass-client # best to use this with the kvmfr module for better performance if passing a dedicated gpu and using an igpu on the host
   ];
   programs.virt-manager.enable = true;
 }
