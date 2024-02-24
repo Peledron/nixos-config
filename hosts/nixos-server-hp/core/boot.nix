1eb0ff6fe0f0f8efb65996c656ab674ec1f547c3
pengolodh <lysander.deloore@gmail.com>
2024-02-24 15:09:46 +0100

    changed server config, the problem with logrotate was fixed by switching away from the hardened kernel

--- hosts/nixos-server-hp/core/boot.nix
+++ hosts/nixos-server-hp/core/boot.nix
@@ -1,13 +1,20 @@
-{ config, lib, pkgs, system, inputs, disks, ... }:   
-
 {
-    boot = {
-        #kernelPackages = pkgs.linuxKernel.packages.linux_hardened; # kernel to be used --> i get a message saying: "extend" missing so ill ignore it for now ==> fixed by using  pkgs.linuxKernel.**packages**.linux_xanmod_latest instead of  pkgs.linuxKernel.kernels.linux_xanmod_latest (even if it was listed this way in nixos packages)
-        kernelParams = [ "loglevel=3" ]; # kernel parameters used at boot
-        # loglevel 3 disables annoying message spam
-        loader = {
-            grub.device = builtins.elemAt disks 0;
-        };
+  config,
+  lib,
+  pkgs,
+  system,
+  inputs,
+  disks,
+  ...
+}: {
+  boot = {
+    #kernelPackages = pkgs.linuxKernel.packages.linux_hardened; # kernel to be used --> i get a message saying: "extend" missing so ill ignore it for now ==> fixed by using  pkgs.linuxKernel.**packages**.linux_xanmod_latest instead of  pkgs.linuxKernel.kernels.linux_xanmod_latest (even if it was listed this way in nixos packages)
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelParams = ["loglevel=3"]; # kernel parameters used at boot
+    # loglevel 3 disables annoying message spam
+    loader = {
+      grub.device = builtins.elemAt disks 0;
     };
-    # for secure boot see: https://github.com/nix-community/lanzaboote/blob/master/docs/QUICK_START.md
+  };
+  # for secure boot see: https://github.com/nix-community/lanzaboote/blob/master/docs/QUICK_START.md
 }
