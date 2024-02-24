dfdca7ff3af08389d64732f0aa57013d160c2ce7
pengolodh <lysander.deloore@gmail.com>
2024-02-24 15:14:33 +0100

    changed server config, fixed an issue with sshd failing on startup

--- hosts/nixos-server-hp/system/services.nix
+++ hosts/nixos-server-hp/system/services.nix
@@ -1,11 +1,14 @@
 # important system services
-
-{ config, lib, pkgs, ... }:
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   # disable autosuspend on lid close so we can use the laptop as a server
   services.logind.lidSwitch = "ignore";
   services.auto-cpufreq.enable = true;
   #services.logrotate.checkConfig = false; # workaround for a bug
 
-  systemd.services.sshd.after = [ "network-interfaces.target" ]; # sets sshd to boot after network, otherwise it fails at startup due to listenAddress not existing yet
+  systemd.services.sshd.after = ["network.target"]; # sets sshd to boot after network, otherwise it fails at startup due to listenAddress not existing yet
 }
