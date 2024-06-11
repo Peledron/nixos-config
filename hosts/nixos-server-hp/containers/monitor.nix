8bbd33b72b98b2f08965b8d312f9567825e1b280
pengolodh <lysander.deloore@gmail.com>
2024-06-11 14:46:24 +0200

    fixes to server-hp

--- hosts/nixos-server-hp/containers/monitor.nix
+++ hosts/nixos-server-hp/containers/monitor.nix
@@ -16,13 +16,20 @@ in {
     #localAddress = "172.64.1.2/24";
     #hostAddress6 = "fc00::1";
     #localAddress6 = "fc00::2";
-    
-    forwardPorts = [{
-      protocol = "tcp";
-      hostPort = 80;
-      containerPort = 80;
-    }];
-    
+    /*
+    forwardPorts = [
+      {
+        protocol = "tcp";
+        hostPort = 80;
+        containerPort = 80;
+      }
+      {
+        protocol = "tcp";
+        hostPort = 443;
+        containerPort = 443;
+      }
+    ];
+    */
     config = {
       config,
       pkgs,
@@ -30,7 +37,7 @@ in {
       ...
     }: {
       #environment.etc."resolv.conf".text = "nameserver 1.1.1.1";# resolv.conf cannot be shared with host
-      
+
       services.resolved.enable = true;
       networking = {
         useDHCP = lib.mkForce true;
@@ -41,7 +48,7 @@ in {
         };
         useHostResolvConf = lib.mkForce false;
       };
-      
+
       #============#
       # nginx:
       #============#
