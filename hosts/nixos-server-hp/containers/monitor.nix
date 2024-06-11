c323230156e407498efaeb9b28597957a760f97e
pengolodh <lysander.deloore@gmail.com>
2024-06-11 04:08:59 +0200

    fixes to server-hp

--- hosts/nixos-server-hp/containers/monitor.nix
+++ hosts/nixos-server-hp/containers/monitor.nix
@@ -206,7 +206,7 @@ in {
             max_chunk_age = "1h";
             chunk_target_size = 999999;
             chunk_retain_period = "30s";
-            max_transfer_retries = 0;
+            #max_transfer_retries = 0;
           };
 
           schema_config = {
@@ -242,9 +242,9 @@ in {
             reject_old_samples_max_age = "168h";
           };
 
-          chunk_store_config = {
-            max_look_back_period = "0s";
-          };
+          #chunk_store_config = {
+          #  max_look_back_period = "0s";
+          #};
 
           table_manager = {
             retention_deletes_enabled = false;
@@ -253,7 +253,7 @@ in {
 
           compactor = {
             working_directory = "/var/lib/loki";
-            shared_store = "filesystem";
+            #shared_store = "filesystem";
             compactor_ring = {
               kvstore = {
                 store = "inmemory";
--- hosts/nixos-server-hp/core/ephemeral.nix
+++ hosts/nixos-server-hp/core/ephemeral.nix
@@ -1,80 +1,92 @@
-{ config, lib, pkgs, system, inputs, impermanence, disko, disks, ... }:
 {
-    # clear root subvolume on each boot as per https://grahamc.com/blog/erase-your-darlings/ and https://nixos.wiki/wiki/Btrfs
-    # Note `lib.mkBefore` is used instead of `lib.mkAfter` here.
-    boot.initrd.postDeviceCommands = pkgs.lib.mkBefore ''
-        mkdir -p /mnt
+  config,
+  lib,
+  pkgs,
+  system,
+  inputs,
+  impermanence,
+  disko,
+  disks,
+  ...
+}: {
+  # clear root subvolume on each boot as per https://grahamc.com/blog/erase-your-darlings/ and https://nixos.wiki/wiki/Btrfs
+  # Note `lib.mkBefore` is used instead of `lib.mkAfter` here.
+  boot.initrd.postDeviceCommands = pkgs.lib.mkBefore ''
+    mkdir -p /mnt
 
-        # We first mount the btrfs root to /mnt
-        # so we can manipulate btrfs subvolumes.
-        mount -o subvol=/ ${builtins.elemAt disks 1} /mnt
+    # We first mount the btrfs root to /mnt
+    # so we can manipulate btrfs subvolumes.
+    mount -o subvol=/ ${builtins.elemAt disks 1} /mnt
 
-        # While we're tempted to just delete /root and create
-        # a new snapshot from /root-blank, /root is already
-        # populated at this point with a number of subvolumes,
-        # which makes `btrfs subvolume delete` fail.
-        # So, we remove them first.
-        #
-        # /root contains subvolumes:
-        # - /root/var/lib/portables
-        # - /root/var/lib/machines
-        #
-        # I suspect these are related to systemd-nspawn, but
-        # since I don't use it I'm not 100% sure.
-        # Anyhow, deleting these subvolumes hasn't resulted
-        # in any issues so far, except for fairly
-        # benign-looking errors from systemd-tmpfiles.
-        btrfs subvolume list -o /mnt/root |
-        cut -f9 -d' ' |
-        while read subvolume; do
-        echo "deleting /$subvolume subvolume..."
-        btrfs subvolume delete "/mnt/$subvolume"
-        done &&
-        echo "deleting /root subvolume..." &&
-        btrfs subvolume delete /mnt/root
+    # While we're tempted to just delete /root and create
+    # a new snapshot from /root-blank, /root is already
+    # populated at this point with a number of subvolumes,
+    # which makes `btrfs subvolume delete` fail.
+    # So, we remove them first.
+    #
+    # /root contains subvolumes:
+    # - /root/var/lib/portables
+    # - /root/var/lib/machines
+    #
+    # I suspect these are related to systemd-nspawn, but
+    # since I don't use it I'm not 100% sure.
+    # Anyhow, deleting these subvolumes hasn't resulted
+    # in any issues so far, except for fairly
+    # benign-looking errors from systemd-tmpfiles.
+    btrfs subvolume list -o /mnt/root |
+    cut -f9 -d' ' |
+    while read subvolume; do
+    echo "deleting /$subvolume subvolume..."
+    btrfs subvolume delete "/mnt/$subvolume"
+    done &&
+    echo "deleting /root subvolume..." &&
+    btrfs subvolume delete /mnt/root
 
-        echo "restoring blank /root subvolume..."
-        btrfs subvolume snapshot /mnt/root-blank /mnt/root
+    echo "restoring blank /root subvolume..."
+    btrfs subvolume snapshot /mnt/root-blank /mnt/root
 
-        # Once we're done rolling back to a blank snapshot,
-        # we can unmount /mnt and continue on the boot process.
-        umount /mnt
-    ''; # from https://mt-caret.github.io/blog/posts/2020-06-29-optin-state.html
+    # Once we're done rolling back to a blank snapshot,
+    # we can unmount /mnt and continue on the boot process.
+    umount /mnt
+  ''; # from https://mt-caret.github.io/blog/posts/2020-06-29-optin-state.html
 
-    environment.persistence."/persist" = {
-        directories = [
-            "/etc/libvirt"
-            "/var/lib/docker"
-            "/var/lib/upower"
-        ];
-        files = [
-            "/etc/machine-id"
-            "/etc/adjtime"
-        ];
-    };
+  environment.persistence."/persist" = {
+    directories = [
+      "/etc/libvirt"
+      "/var/lib/docker"
+      "/var/lib/upower"
+    ];
+    files = [
+      "/etc/machine-id"
+      "/etc/adjtime"
+    ];
+  };
 
-    # disable sudo lecture, it is the warning message that displays the first time you use the sudo command
-    security.sudo.extraConfig = ''
-        # rollback results in sudo lectures after each reboot
-        Defaults lecture = never
-    '';
-    services.openssh = {
-        enable = true;
-        # sets nix to use the host keys in the given directory
-        hostKeys = [
-            {
-                path = "/persist/ssh/ssh_host_ed25519_key";
-                type = "ed25519";
-            }
-            {
-                path = "/persist/ssh/ssh_host_rsa_key";
-                type = "rsa";
-                bits = 4096;
-            }
-        ];
-    };
-    /*networking.wireguard.interfaces.wg0 = {
-            generatePrivateKeyFile = true;
-            privateKeyFile = "/persist/etc/wireguard/wg0";
-    };*/ # uncomment if wireguard is used
+  # disable sudo lecture, it is the warning message that displays the first time you use the sudo command
+  security.sudo.extraConfig = ''
+    # rollback results in sudo lectures after each reboot
+    Defaults lecture = never
+  '';
+  services.openssh = {
+    enable = true;
+    # sets nix to use the host keys in the given directory
+    hostKeys = [
+      {
+        path = "/persist/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+      {
+        path = "/persist/ssh/ssh_host_rsa_key";
+        type = "rsa";
+        bits = 4096;
+      }
+    ];
+  };
+  /*
+    networking.wireguard.interfaces.wg0 = {
+          generatePrivateKeyFile = true;
+          privateKeyFile = "/persist/etc/wireguard/wg0";
+  };
+  */
+  # uncomment if wireguard is used
 }
