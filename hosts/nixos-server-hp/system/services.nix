6a59a547c20fc6de4c8e633f1ad2c3897e0e81fd
pengolodh <lysander.deloore@gmail.com>
2024-06-14 11:08:30 +0200

    added podman to nixos-server-hp service imports

--- hosts/nixos-server-hp/system/services.nix
+++ hosts/nixos-server-hp/system/services.nix
@@ -5,11 +5,12 @@
   pkgs,
   ...
 }: {
+  imports = [(self + "/global/modules/virt/podman.nix")];
+  
   # disable autosuspend on lid close so we can use the laptop as a server
   services.logind.lidSwitch = "ignore";
   services.auto-cpufreq.enable = true;
   #services.logrotate.checkConfig = false; # workaround for a bug
 
   systemd.services.sshd.after = ["network.target" "systemd-networkd.service"]; # sets sshd to boot after network, otherwise it fails at startup due to listenAddress not existing yet
-  
 }
