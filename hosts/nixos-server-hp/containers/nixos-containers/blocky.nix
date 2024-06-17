7d4d05c11e0320e05e4bd6f58606fd1080f84e99
pengolodh <lysander.deloore@gmail.com>
2024-06-17 16:42:11 +0200

    blocky config fixed, resolvd was using port 53

--- hosts/nixos-server-hp/containers/nixos-containers/blocky.nix
+++ hosts/nixos-server-hp/containers/nixos-containers/blocky.nix
@@ -20,6 +20,7 @@ in {
       lib,
       ...
     }: {
+      services.resolved.enable = lib.mkForce false;
       networking = {
         interfaces = {
           ${netport}.ipv4.addresses = [
@@ -42,73 +43,73 @@ in {
         };
         useHostResolvConf = lib.mkForce false;
       };
-      services.blocky = {
-        enable = true;
-        settings = {
-          # adapted from https://bayas.dev/posts/blocky-adblock-docker-setup
-          upstreams = {
-            groups = {
-              default = [
-                # unecrypted
-                "1.1.1.2"
-                "1.0.0.2"
-                # encrypted DoH
-                "https://dns.nextdns.io"
-                "https://1.1.1.2/dns-query"
-              ];
-            };
-            # blocky will pick the 2 fastest upstreams but you can also use the `strict` strategy
-            strategy = "parallel_best";
-            timeout = "2s";
+    };
+    services.blocky = {
+      enable = true;
+      settings = {
+        # adapted from https://bayas.dev/posts/blocky-adblock-docker-setup
+        upstreams = {
+          groups = {
+            default = [
+              # unecrypted
+              "1.1.1.2"
+              "1.0.0.2"
+              # encrypted DoH
+              "https://dns.nextdns.io"
+              "https://1.1.1.2/dns-query"
+            ];
           };
-          # check if upstreams are working
-          #init.strategy = true;
-          # we will not be using ipv6 for now
-          connectIPVersion = "v4";
-
-          blocking = {
-            # I prefer the HaGeZi Light blocklist for set and forget setup, you can use any other blacklist nor whitelist you want
-            # Blocky supports hosts, domains and regex syntax
-            blackLists = {
-              ads = [
-                "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/multi.txt"
-              ];
-            };
-
-            clientGroupsBlock = {
-              default = [
-                "ads"
-              ];
-            };
-            blockType = "zeroIp";
-            blockTTL = "1m";
-            loading = {
-              refreshPeriod = "6h";
-              downloads = {
-                timeout = "60s";
-                attempts = 5;
-
-                cooldown = "10s";
-              };
-              concurrency = 16;
-              # start answering queries immediately after start
-              strategy = "fast";
-              maxErrorsPerSource = 5;
-            };
-          };
-          caching = {
-            # enable prefetching improves performance for often used queries
-            prefetching = true;
-            # if a domain is queried more than prefetchThreshold times, it will be prefetched for prefetchExpires time
-            prefetchExpires = "24h";
-            prefetchThreshold = 2;
-          };
-          # use encrypted DNS for resolving upstreams
-          # same syntax as normal upstreams
-          bootstrapDns = [
-            "https://1.1.1.2/dns-query"
-          ];
+          # blocky will pick the 2 fastest upstreams but you can also use the `strict` strategy
+          strategy = "parallel_best";
+          timeout = "2s";
         };
+        # check if upstreams are working
+        #init.strategy = true;
+        # we will not be using ipv6 for now
+        connectIPVersion = "v4";
+
+        blocking = {
+          # I prefer the HaGeZi Light blocklist for set and forget setup, you can use any other blacklist nor whitelist you want
+          # Blocky supports hosts, domains and regex syntax
+          blackLists = {
+            ads = [
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/multi.txt"
+            ];
+          };
+
+          clientGroupsBlock = {
+            default = [
+              "ads"
+            ];
+          };
+          blockType = "zeroIp";
+          blockTTL = "1m";
+          loading = {
+            refreshPeriod = "6h";
+            downloads = {
+              timeout = "60s";
+              attempts = 5;
+
+              cooldown = "10s";
+            };
+            concurrency = 16;
+            # start answering queries immediately after start
+            strategy = "fast";
+            maxErrorsPerSource = 5;
+          };
+        };
+        caching = {
+          # enable prefetching improves performance for often used queries
+          prefetching = true;
+          # if a domain is queried more than prefetchThreshold times, it will be prefetched for prefetchExpires time
+          prefetchExpires = "24h";
+          prefetchThreshold = 2;
+        };
+        # use encrypted DNS for resolving upstreams
+        # same syntax as normal upstreams
+        bootstrapDns = [
+          "https://1.1.1.2/dns-query"
+        ];
       };
     };
   };
