486bfb824ddca075f3df74415b99c9aae538631f
pengolodh <lysander.deloore@gmail.com>
2024-06-26 21:08:19 +0200

    adguard service setup

--- hosts/nixos-server-hp/containers/nixos-containers/adguard-home.nix
+++ hosts/nixos-server-hp/containers/nixos-containers/adguard-home.nix
@@ -17,6 +17,10 @@ in {
     #extraFlags = ["-U"]; # run as user instead of root
     privateNetwork = true;
     hostBridge = "${br_local_container_name}";
+    "/var/lib/querrylog" = {
+      hostPath = "${containerpath}/querrylog";
+      isReadOnly = false;
+    };
     config = {
       config,
       pkgs,
@@ -46,7 +50,7 @@ in {
         useNetworkd = true;
         firewall = {
           enable = true;
-          allowedTCPPorts = [53 4000];
+          allowedTCPPorts = [53 443];
           allowedUDPPorts = [53];
         };
         useHostResolvConf = lib.mkForce false;
@@ -55,9 +59,30 @@ in {
         enable = true;
         openFirewall = true; # opens port associated with port option (3000 by default)
         mutableSettings = true; # Allow changes made on the AdGuard Home web interface to persist between service restarts.
-        settings  = {
+        settings = {
           # you can add more settings here, they are merged with exisiting settings form webui
           # https://github.com/AdguardTeam/AdGuardHome/wiki/Configuration#configuration-file
+          users = {
+            name = "pengolodh";
+            password = "Adguard@Home";
+          };
+          upstream_dns = [
+            "tls://fdns1.dismail.de:853"
+            "tls://fdns2.dismail.de:853"
+          ];
+          upstream_mode = "parallel";
+          bootstrap_dns = [
+            "https://1.1.1.2/dns-query"
+            "https://1.0.0.2/dns-query"
+          ];
+          enable_dnssec = true;
+          querrylog.dir_path = "/var/lib/querrylog";
+          tls = {
+            enabled = true;
+            server_name = "adguard.home.pengolodh.be";
+            force_https = true;
+            port_https = 443;
+          };
         };
       };
     };
