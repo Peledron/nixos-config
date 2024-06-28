5e6c3ef5d8f5a41d260614dfceba955204e95c03
pengolodh <lysander.deloore@gmail.com>
2024-06-28 15:43:59 +0200

    test

--- global/users/pengolodh/home/global/configs/fish.nix
+++ global/users/pengolodh/home/global/configs/fish.nix
@@ -21,7 +21,7 @@
       end
     '';
     functions = {
-      push-config = "cd $FLAKE; git add --all; git commit -m $argv; git push;";
+      push-config = ''pushd $FLAKE; git add --all; git commit -m "$argv"; git push; popd'';
     };
   };
   # ---
