4994a8bf35008008a40cb0fb2efb54c987cc7d63
pengolodh <lysander.deloore@gmail.com>
2024-07-21 00:52:01 +0200

    fixed a minor error, might be the culprit

--- hosts/default.nix
+++ hosts/default.nix
@@ -127,7 +127,7 @@
           # Additional modules specific to this host
           extraModules = [
             # Import the host-specific configuration
-            "${./hosts}/${name}"
+            "${self}/hosts/${name}"
             # Apply any extra configuration passed to mkHostConfig
             extraConfig
             # Create user configuration for "pengolodh"
