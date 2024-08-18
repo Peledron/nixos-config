b11d769eb71553a36e4c88ee41d59354aee1806f
pengolodh <lysander.deloore@gmail.com>
2024-08-19 00:25:10 +0200

    added gamingMouse file to global hardware modules

--- hosts/nixos-main/core/hardware.nix
+++ hosts/nixos-main/core/hardware.nix
@@ -11,7 +11,8 @@
     ++ [(self + "/global/modules/hardware/amdGpu.nix")]
     ++ [(self + "/global/modules/hardware/bluetooth.nix")]
     ++ [(self + "/global/modules/hardware/drawingTablet.nix")]
-    ++ [(self + "/global/modules/hardware/keychronKeyboard.nix")];
+    ++ [(self + "/global/modules/hardware/keychronKeyboard.nix")]
+    ++ [(self + "/global/modules/hardware/gamingMouse.nix")];
 
   # base:
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
